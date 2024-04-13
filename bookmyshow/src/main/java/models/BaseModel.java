65ec195dec51852a08448ea3c50436be379ae20c
Bhushan Talele <bhushantalele2298@gmail.com>
2024-04-13 14:03:28 +0530

    Added Auto auditing annotations

--- bookmyshow/src/main/java/models/BaseModel.java
+++ bookmyshow/src/main/java/models/BaseModel.java
@@ -1,17 +1,22 @@
 package models;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 
 public class BaseModel {
 
@@ -21,6 +26,9 @@ public class BaseModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;  //PrimaryKey
 
+    @CreatedDate
     private Date createdAt;
+
+    @LastModifiedDate
     private Date lastModifiedAt;
 }
